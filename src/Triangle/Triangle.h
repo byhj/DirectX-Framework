ad1e7e20d533217b0654867eb379750591abfb8f
byhj <476652422@qq.com>
2015-11-24 15:13:21 +0800

    add triangle

---
+++ src/Triangle/Triangle.h
@@ -0,0 +1,20 @@
+#ifndef Triangle_H
+#define Triangle_H
+
+namespace byhj
+{
+	class Triangle
+	{
+	public:
+		Triangle();
+		~Triangle();
+
+		void Init();
+		void Update();
+		void Render();
+		void Shutdown(;
+
+	private:
+	};
+}
+#endif
